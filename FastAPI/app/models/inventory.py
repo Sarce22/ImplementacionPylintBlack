5e7e17250d52ed590b1a48039c9a152f0a2f2226
Sarce22 <sebastianarce482@gmail.com>
2024-09-14 15:44:43 -0500

    addModelInventory

---
+++ FastAPI/app/models/inventory.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class inventory(BaseModel):
+    """
+    Represents an inventory item.
+
+    Attributes:
+        id (int): The unique identifier of the inventory item.
+        shop_id (int): The identifier of the shop that owns the inventory item.
+        product_id (int): The identifier of the product that the inventory item represents.
+        quantity (int): The quantity of the product in the inventory.
+    """
+    id: int
+    shop_id: int
+    product_id: int
+    quantity: int
