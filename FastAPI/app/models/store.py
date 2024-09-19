2734cbb026c21a8598569cdb60ed98ddcd0ac398
JPablo67 <juanpa.995@gmail.com>
2024-09-19 17:53:45 -0500

    Docker files

--- FastAPI/app/models/store.py
+++ FastAPI/app/models/store.py
@@ -2,6 +2,14 @@ from pydantic import BaseModel
 
 
 class Store(BaseModel):
-    id: str
-    nombre: str
-    direccion: str
+    """
+    Represents an Store item.
+
+    Attributes:
+        id (int): The unique identifier of the store item.
+        name (store): The identifier of the shop that owns the inventory item.
+        address (str): The identifier of the product that the inventory item represents.
+    """
+    id: int
+    name: str
+    address: str
